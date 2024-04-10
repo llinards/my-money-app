43dc1977d4350ad88267d645814d4c8ec50a04be
Linards <9407839+llinards@users.noreply.github.com>
2024-04-10 09:51:32 +0300

    Basic project setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Linards Lazdiņš',
+            'email' => 'linards@linards.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
